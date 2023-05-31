ca4cb84058c78dfed08bbd19e24617cb4984c4be
ytg77 <yashpreyas.kulkarni@yahoo.com>
2023-05-31 18:09:38 +0530

    First

---
+++ UI.py
@@ -0,0 +1,6 @@
+import currentGameFinder as cgf
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Free Games this week')
+window.geometry("500x500")
---
+++ currentGameFinder.py
@@ -0,0 +1,25 @@
+
+import keys
+import requests
+from pprint import pprint
+
+url = "https://free-epic-games.p.rapidapi.com/free"
+
+headers = {
+	"X-RapidAPI-Key": keys.rapidAPIKey,
+	"X-RapidAPI-Host": "free-epic-games.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers)
+resJSON = response.json()
+
+currentName = []
+currentImage = []
+for i in resJSON['freeGames']['current']:
+    currentName.append(i['title'])
+    currentImage.append(i['keyImages'][0]['url'])
+# pprint(resJSON)
+# resp = json.loads(response)
+print(currentName)
+print(currentImage)
+# print(response.headers)
